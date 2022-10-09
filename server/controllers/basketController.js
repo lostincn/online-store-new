0a2d0c3ac1682ef305367893ee99e66ad89fccb0
lostincn <aidaroffski1@gmail.com>
2022-10-09 15:44:48 +0600

    editing basketController

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -3,7 +3,7 @@ const { ApiError } = require("../error/ApiError");
 
 class BasketController{
     try {
-        
+        console.log(`updating GH repository`)
     } catch (error) {
         next(ApiError.badRequest(error.message))
     }
